3e67bfbfbfa5d3ece658f437f02f248ee5434208
Даниил Никитин <faverus061999@gmail.com>
2023-09-12 15:31:43 +0300

    feat: start

---
+++ src/app/components/FirstBlock/FirstBlock.tsx
@@ -0,0 +1,7 @@
+import style from './FirstBlock.module.scss';
+
+const FirstBlock = () => {
+  return <section className={style.block}></section>;
+};
+
+export default FirstBlock;
---
+++ src/app/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import style from './Header.module.scss';
+
+const Header = () => {
+  return (
+    <header className={style.header}>
+      <nav>
+        <a href="">О Нас</a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;

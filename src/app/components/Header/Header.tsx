1fd3f7f4f5ddd7f42dd6a00a21a60df566c7394e
Даниил Никитин <faverus061999@gmail.com>
2023-09-12 15:41:37 +0300

    tel in header

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,11 +1,15 @@
 import style from './Header.module.scss';
+import logo from '../../../../public/logo.svg';
+import Image from 'next/image';
 
 const Header = () => {
   return (
     <header className={style.header}>
+      <Image src={logo} alt="Логотип" width={100} height={100} />
       <nav>
         <a href="">О Нас</a>
       </nav>
+      <a href="tel:+79531613787">89531613787</a>
     </header>
   );
 };

38403a089ce8ce3f5860de5c8bb89a7d57f91b60
Даниил Никитин <faverus061999@gmail.com>
2023-09-14 17:38:43 +0300

    feat

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,19 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/icon.ico" />
+        <meta property="og:url" content="logocite.vercel.app" />
+        <meta
+          property="og:title"
+          content="Официальный сайт Мастерской формирования чёткой речи ЛОГОПЕДОВ из CHICAGA"
+        />
+        <meta
+          name="keywords"
+          content="Логопед Логопеды Логопеды озерки Логопеды Академка Логопеды Академическая Логопеды Энгельса Логопед Науки Логопед Соколова Логопеды в СПб Логопедические услуги"
+        />
+        <meta
+          property="og:description"
+          content="Мастерская формирования чёткой речи ЛОГОПЕДОВ из CHICAGA усиленно работает над устранением любых ошибок в речи ребёнка, а также над формированием ясных мыслей и выразительной речи ребёнка."
+        />
       </head>
       <body>{children}</body>
     </html>

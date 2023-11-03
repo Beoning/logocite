3a89ef29d1f85a6e48dc3c2eb934545ac2796a1d
Даниил Никитин <faverus061999@gmail.com>
2023-11-03 16:40:22 +0300

    media

--- src/app/components/Slider/Swiper/SwiperComponent.tsx
+++ src/app/components/Slider/Swiper/SwiperComponent.tsx
@@ -1,9 +1,9 @@
 'use client';
 
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperRef, SwiperSlide } from 'swiper/react';
 import style from './SwiperComponent.module.scss';
 import { Autoplay, Navigation, Pagination } from 'swiper/modules';
-import { FC } from 'react';
+import { FC, ReactElement, useLayoutEffect, useRef, useState } from 'react';
 import { StaticImageData } from 'next/image';
 import Slide from '../Slide/Slide';
 import { useSwiper } from 'swiper/react';
@@ -14,10 +14,18 @@ import 'swiper/css/navigation';
 const SwiperComponent: FC<{
   images: StaticImageData[];
 }> = ({ images }) => {
-  const swiper = useSwiper();
+  const ref = useRef<null | SwiperRef>(null);
+
+  const [width, setWidth] = useState(0);
+
+  useLayoutEffect(() => {
+    //@ts-ignore
+    setWidth(ref.current?.offsetWidth);
+  }, []);
 
   return (
     <Swiper
+      ref={ref}
       style={{
         //@ts-ignore
         '--swiper-navigation-color': '#fff',
@@ -30,7 +38,7 @@ const SwiperComponent: FC<{
       loop
       enabled={true}
       spaceBetween={50}
-      slidesPerView={3}
+      slidesPerView={width > 400 ? 3 : 1}
       pagination={{
         dynamicBullets: true,
         clickable: true,
